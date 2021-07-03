d6cce20dfdf7eb5cf0d5c6e3cff89b099de57f16
opas350 <opas350@gmail.com>
2021-07-02 22:30:27 -0500

    Add Files

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,23 @@ use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
+use sqlx::postgres::PgPoolOptions;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string())
         .expect("Failed to connect to Postgres.");
-    let address = format!("0.0.0.0:{}", configuration.application_port);
+    // let address = format!("0.0.0.0:{}", configuration.application_port);
+    // let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     println!(
         "Server running on: http://{:?}",
